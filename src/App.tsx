f5edbf77dbbc0303966ab5fa8c1059930e4e0528
SF <sf.chang232tpv@gmail.com>
2026-05-12 16:21:20 +0800

    modified 0512-26 16:21

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import SnakeGame from './components/SnakeGame';
 
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 export default function App() {
+
   return (
     <main>
       <SnakeGame />
